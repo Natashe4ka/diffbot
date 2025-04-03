c7f8f284176047a699e3a77496a9ae48ddb1751f
Natashe4ka <safonova.nk@gmail.com>
2025-04-03 20:09:09 +0300

    cyphal injected

--- hardware/diffbot_hardware.cpp
+++ hardware/diffbot_hardware.cpp
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include "diffbot_hardware.hpp"
+#include "comms.h"
 
 #include <chrono>
 #include <cmath>
@@ -26,6 +27,100 @@
 #include "hardware_interface/lexical_casts.hpp"
 #include "hardware_interface/types/hardware_interface_type_values.hpp"
 #include "rclcpp/rclcpp.hpp"
+
+#include "cyphal/allocators/o1/o1_allocator.h"
+#include "cyphal/cyphal.h"
+#include "cyphal/providers/LinuxCAN.h"
+#include "cyphal/subscriptions/subscription.h"
+#include "uavcan/node/Heartbeat_1_0.h"
+#include "reg/udral/physics/kinematics/rotation/Planar_0_1.h"
+#include "reg/udral/physics/kinematics/cartesian/Twist_0_1.h"
+#include "reg/udral/physics/kinematics/cartesian/State_0_1.h"
+
+#include <uavcan/_register/Access_1_0.h>
+#include <uavcan/_register/List_1_0.h>
+
+
+TYPE_ALIAS(Twist, reg_udral_physics_kinematics_cartesian_Twist_0_1)
+TYPE_ALIAS(HBeat, uavcan_node_Heartbeat_1_0)
+TYPE_ALIAS(JS_msg, reg_udral_physics_kinematics_rotation_Planar_0_1)
+TYPE_ALIAS(State, reg_udral_physics_kinematics_cartesian_State_0_1)
+
+TYPE_ALIAS(RegisterListRequest, uavcan_register_List_Request_1_0)
+TYPE_ALIAS(RegisterListResponse, uavcan_register_List_Response_1_0)
+
+TYPE_ALIAS(RegisterAccessRequest, uavcan_register_Access_Request_1_0)
+TYPE_ALIAS(RegisterAccessResponse, uavcan_register_Access_Response_1_0)
+
+std::byte buffer[sizeof(CyphalInterface) + sizeof(LinuxCAN) + sizeof(O1Allocator)];
+
+void error_handler() {std::cout << "error" << std::endl; std::exit(EXIT_FAILURE);}
+uint64_t micros_64() {
+    using namespace std::chrono;
+    return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+}
+UtilityConfig utilities(micros_64, error_handler);
+
+std::shared_ptr<CyphalInterface> cy_interface;
+
+uint32_t uptime = 0;
+void heartbeat() {
+    static CanardTransferID hbeat_transfer_id = 0;
+    HBeat::Type heartbeat_msg = {.uptime = uptime, .health = {uavcan_node_Health_1_0_NOMINAL}, .mode = {uavcan_node_Mode_1_0_OPERATIONAL}};
+    cy_interface->send_msg<HBeat>(&heartbeat_msg, uavcan_node_Heartbeat_1_0_FIXED_PORT_ID_, &hbeat_transfer_id);
+    uptime += 1;
+}
+
+
+static float w_pos[2] = {0.0};
+static float w_vel[2] = {0.0};
+
+class JSReader: public AbstractSubscription<JS_msg> {
+public:
+    JSReader(InterfacePtr interface): AbstractSubscription<JS_msg>(
+      interface,
+      AGENT_JS_SUB_PORT
+    ) {};
+    void handler(const reg_udral_physics_kinematics_rotation_Planar_0_1& js_read, CanardRxTransfer* transfer) override {
+      
+        //std::cout << "Node id: " << +transfer->metadata.remote_node_id << std::endl;
+        w_pos[transfer->metadata.remote_node_id-1] = js_read.angular_position.radian;
+        w_vel[transfer->metadata.remote_node_id-1] = js_read.angular_velocity.radian_per_second;
+    }
+};
+JSReader * JS_reader;
+
+
+static CanardTransferID int_transfer_id = 0;
+
+void send_twist_cmd(CanardNodeID node_id, float pos, float vel, float eff) {
+	int_transfer_id++;
+	reg_udral_physics_kinematics_rotation_Planar_0_1 js_msg =
+	{
+			.angular_position = pos,
+			.angular_velocity = vel,
+			.angular_acceleration = eff
+	};
+    cy_interface->send_msg<JS_msg>(
+		&js_msg,
+		sub_port_id[node_id],
+		&int_transfer_id
+	);
+}
+
+class HBeatReader: public AbstractSubscription<HBeat> {
+  public:
+      HBeatReader(InterfacePtr interface): AbstractSubscription<HBeat>(interface,
+          uavcan_node_Heartbeat_1_0_FIXED_PORT_ID_
+      ) {};
+      void handler(const uavcan_node_Heartbeat_1_0& hbeat, CanardRxTransfer* transfer) override {
+          std::cout << +transfer->metadata.remote_node_id << ": " << hbeat.uptime <<  std::endl;
+      }
+  };
+  HBeatReader * reader;
+
+
+
 
 namespace diffbot
 {
@@ -45,6 +140,11 @@ hardware_interface::CallbackReturn DiffBotSystemHardware::on_init(
   hw_stop_sec_ =
     hardware_interface::stod(info_.hardware_parameters["example_param_hw_stop_duration_sec"]);
   // END: This part here is for exemplary purposes - Please do not copy to your production code
+
+  cy_interface = CyphalInterface::create_heap<LinuxCAN, O1Allocator>(100, "can0", 1000, utilities); //Node ID, transport, queue_len, utilities
+  reader = new HBeatReader(cy_interface);
+  JS_reader = new JSReader(cy_interface);
+
 
   for (const hardware_interface::ComponentInfo & joint : info_.joints)
   {
